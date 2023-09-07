cd3d4aad17da61ed1710a5e2251b11eeb58c1094
pmoranc <pablomoranc@gmail.com>
2023-09-07 15:47:51 -0500

    add action link

--- src/main.php
+++ src/main.php
@@ -42,6 +42,20 @@ function link_crawl_load_plugin() {
 	$link_crawl->init();
 }
 
+/**
+ * Add action link to plugin page
+ *
+ * @param array $links Action links of plugins.
+ */
+function link_crawl_add_plugin_action_link( $links ) {
+	$url           = add_query_arg( 'page', 'link-crawl', admin_url( 'admin.php' ) );
+	$settings_link = '<a class="calendar-link" href="' . esc_url( $url ) . '">' . esc_html__( 'Settings', 'link_crawl' ) . '</a>';
+	array_unshift( $links, $settings_link );
+
+	return $links;
+}
+
+add_filter( 'plugin_action_links_plugin-crawl/link-crawl.php', 'link_crawl_add_plugin_action_link' );
 add_action( 'plugins_loaded', 'link_crawl_load_plugin' );
 register_activation_hook( LINK_CRAWL_FILE, 'link_crawl_activate_plugin' );
 register_deactivation_hook( LINK_CRAWL_FILE, 'link_crawl_deactivate_plugin' );
